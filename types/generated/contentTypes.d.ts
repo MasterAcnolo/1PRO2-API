c568384bdf39c7656613a4a894ab280d8f9b0430
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-03-01 14:16:07 +0100

    Fix: Remove Labels

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -526,30 +526,6 @@ export interface ApiColumnColumn extends Struct.CollectionTypeSchema {
   };
 }
 
-export interface ApiLabelLabel extends Struct.CollectionTypeSchema {
-  collectionName: 'labels';
-  info: {
-    displayName: 'LABEL';
-    pluralName: 'labels';
-    singularName: 'label';
-  };
-  options: {
-    draftAndPublish: false;
-  };
-  attributes: {
-    createdAt: Schema.Attribute.DateTime;
-    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
-      Schema.Attribute.Private;
-    locale: Schema.Attribute.String & Schema.Attribute.Private;
-    localizations: Schema.Attribute.Relation<'oneToMany', 'api::label.label'> &
-      Schema.Attribute.Private;
-    publishedAt: Schema.Attribute.DateTime;
-    updatedAt: Schema.Attribute.DateTime;
-    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
-      Schema.Attribute.Private;
-  };
-}
-
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -1063,7 +1039,6 @@ declare module '@strapi/strapi' {
       'api::board.board': ApiBoardBoard;
       'api::card.card': ApiCardCard;
       'api::column.column': ApiColumnColumn;
-      'api::label.label': ApiLabelLabel;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
